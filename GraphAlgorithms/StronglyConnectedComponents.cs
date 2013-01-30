c69e2f5d7f6d566249b7f0bfcfad25507c8e4c52
Feras <feras.ahmed@live.com>
2013-01-30 17:00:32 +0200

    Strongly Connected Components
    Solving Strongly Connected Components problem using Tarjan Algorithm.

---
+++ GraphAlgorithms/StronglyConnectedComponents.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgorithmsLibrary.GraphAlgorithms
+{
+    //Using Tarjan SCC Algorithm
+    class StronglyConnectedComponents
+    {
+        const int WHITE = 0;
+        const int BLACK = 1;
+        const int GRAY = 2;
+        List<List<int>> AdjList;
+        List<List<int>> SccComponenets;
+        Stack<int> visitedNodes;
+        int[] vertex_num, vertex_low, visited;
+        int vertexNumber;
+
+        public StronglyConnectedComponents()
+        {
+            AdjList = null;
+            visited = vertex_num = vertex_low = null;
+            visitedNodes = null;
+            SccComponenets = new List<List<int>>();
+            visitedNodes = new Stack<int>();
+        }
+
+        public int Run(List<List<int>> AdjacencyList, int nodesCount)
+        {
+            AdjList = AdjacencyList; 
+            SccComponenets.Clear(); // clear previous components
+            visitedNodes.Clear();
+            visited = new int[nodesCount]; // check whether two vertices are in same component
+            vertex_num = new int [nodesCount];
+            vertex_low = new int[nodesCount];
+            vertexNumber = 0;
+
+            for (int i = 0; i < AdjList.Count; i++)
+                if (vertex_num[i] == 0)
+                    tarjanScc(i);
+
+            return SccComponenets.Count;
+        }
+
+        public void PrintComponents()
+        {
+            if (SccComponenets == null)
+                return;
+
+            foreach (List<int> group in SccComponenets)
+            {
+                Console.WriteLine("\n------------------------");
+                foreach (int vertex in group)
+                    Console.Write(vertex + ", ");
+            }
+        }
+
+        void tarjanScc(int v)
+        {
+            int u = 0;
+            vertex_num[v] = vertex_low[v] = vertexNumber++;
+            visited[v] = GRAY;
+            visitedNodes.Push(v);
+            for (int i = 0; i < AdjList[v].Count; i++)
+            {
+                u = AdjList[v][i];
+
+                if (visited[u] == WHITE) // node isn't visited yet
+                  tarjanScc(u);
+
+                if(visited[u] != BLACK)
+                    vertex_low[v] = Math.Min(vertex_low[v], vertex_low[u]);
+            }
+
+            if (vertex_num[v] == vertex_low[v])
+            {
+                SccComponenets.Add(new List<int>());
+                int index = SccComponenets.Count - 1;
+                Console.WriteLine("Current vertex Number: " + v);
+                while (true)
+                {
+                    u = visitedNodes.Peek();
+                    visitedNodes.Pop();
+                    visited[u] = BLACK;
+                    SccComponenets[index].Add(u);
+                    
+                    if (u == v)
+                        break;
+                }
+            }
+        }
+
+        public bool SameComponent(int v1, int v2)
+        {
+            return (vertex_low != null) && v1 < vertex_low.Length && v2 < vertex_low.Length && (vertex_low[v1] == vertex_low[v2]);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using AlgorithmsLibrary.DynammicComponents;
+using AlgorithmsLibrary.GraphAlgorithms;
 
 namespace AlgorithmsLibrary
 {
@@ -12,6 +13,21 @@ namespace AlgorithmsLibrary
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            StronglyConnectedComponents scc = new StronglyConnectedComponents();
+            List<List<int>> adj = new List<List<int>>();
+
+            adj.Add(new List<int> { 1, 6 }); 
+            adj.Add(new List<int> { 2 });
+            adj.Add(new List<int> { 3 });
+            adj.Add(new List<int> { 4 });
+            adj.Add(new List<int> { 5 });
+            adj.Add(new List<int> { 2 });
+            adj.Add(new List<int> { 1, 7 });
+            adj.Add(new List<int> ());
+
+            Console.WriteLine(scc.Run(adj, 8));
+            scc.PrintComponents();
         }
     }
 }
