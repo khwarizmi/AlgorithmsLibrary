8bbeadf602d9e19fd9e9dcf8492f0661cddba49b
Feras <feras.ahmed@live.com>
2013-01-31 12:05:56 +0200

    Updated

--- GraphAlgorithms/StronglyConnectedComponents.cs
+++ GraphAlgorithms/StronglyConnectedComponents.cs
@@ -14,24 +14,24 @@ namespace AlgorithmsLibrary.GraphAlgorithms
         List<List<int>> AdjList;
         List<List<int>> SccComponenets;
         Stack<int> visitedNodes;
-        int[] vertex_num, vertex_low, visited;
+        int[] vertex_num, vertex_low, visited, vertex_visited;
         int vertexNumber;
 
         public StronglyConnectedComponents()
         {
             AdjList = null;
-            visited = vertex_num = vertex_low = null;
+            visited = vertex_num = vertex_low = vertex_visited = null;
             visitedNodes = null;
             SccComponenets = new List<List<int>>();
             visitedNodes = new Stack<int>();
         }
 
-        public int Run(List<List<int>> AdjacencyList, int nodesCount)
+        public int RunTarjan(List<List<int>> AdjacencyList, int nodesCount)
         {
             AdjList = AdjacencyList; 
             SccComponenets.Clear(); // clear previous components
             visitedNodes.Clear();
-            visited = new int[nodesCount]; // check whether two vertices are in same component
+            visited = new int[nodesCount]; 
             vertex_num = new int [nodesCount];
             vertex_low = new int[nodesCount];
             vertexNumber = 0;
@@ -43,6 +43,45 @@ namespace AlgorithmsLibrary.GraphAlgorithms
             }
             return SccComponenets.Count;
         }
+
+        /*
+         * // More Updates and changed to Algorithm needed
+         * // Runs Algorithm on Graph, Graph Transpose, Nodes Count
+        public int RunKosaraju(List<List<int>> G, List<List<int>> GT, int nodesCount)
+        {
+            AdjList = G;
+            SccComponenets.Clear(); // clear previous components
+            visitedNodes.Clear();
+            visited = new int[nodesCount]; 
+            vertex_num = new int[nodesCount];
+            vertex_visited = new int[nodesCount];
+            vertex_low = new int[nodesCount];
+            vertexNumber = 0;
+
+            //Init array
+            for (int i = 0; i < nodesCount; i++)
+                vertex_low[i] = -(i + 1);
+
+            // First Path of Algorithm
+            for (int i = 0; i < AdjList.Count; i++)
+                if (visited[i] == WHITE)
+                    KosarajuForward(i);
+
+            int v = 0;
+            AdjList = GT;
+            // Second Path on Graph Transpose
+            for (int i = vertex_visited.Length - 1; i >= 0; i--)
+            {
+                v = vertex_visited[i];
+                if(visited[v] == BLACK)
+                    KosarajuBackward(v);
+            }
+
+            for(int i = 0; i < AdjList.Count; i++)
+                Console.WriteLine(vertex_num[i]);
+            return SccComponenets.Count;
+        }
+        */
 
         public void PrintComponents()
         {
@@ -90,6 +129,70 @@ namespace AlgorithmsLibrary.GraphAlgorithms
                 }
             }
         }
+
+        /* BFS Code needs to be Changed to DFS to Solve Problem Correct
+        void KosarajuForward(int v)
+        {
+            int u = 0;
+            Queue<int> nodeList = new Queue<int>();
+            Stack<int> postOrder = new Stack<int>();
+            nodeList.Enqueue(v);
+            visited[v] = BLACK;
+
+            while (nodeList.Count > 0)
+            {
+                v = nodeList.Dequeue();
+                postOrder.Push(v);
+                for (int i = 0; i < AdjList[v].Count; i++)
+                {
+                    u = AdjList[v][i];
+                    if(visited[u] != BLACK)
+                    {
+                        nodeList.Enqueue(u);
+                        visited[u] = BLACK;
+                    }
+                }
+            }
+
+            while (postOrder.Count > 0)
+            {
+                u = postOrder.Pop();
+                vertex_num[u] = ++vertexNumber;
+                vertex_visited[vertexNumber - 1] = u;
+            }
+        }
+
+        void KosarajuBackward(int v)
+        {
+            int u = 0;
+            int maxCost = vertex_num[v];
+            Queue<int> nodeList = new Queue<int>();
+            List<int> group = new List<int>();
+            nodeList.Enqueue(v);
+            visited[v] = GRAY;    
+
+            while (nodeList.Count > 0)
+            {
+                v = nodeList.Dequeue();
+                group.Add(v); // current group Componenets
+                for (int i = 0; i < AdjList[v].Count; i++)
+                {
+                    u = AdjList[v][i];
+                    if (maxCost > vertex_num[u] && visited[u] != GRAY)
+                    {
+                        nodeList.Enqueue(u);
+                        visited[u] = GRAY;        
+                    }
+                }
+            }
+
+            int groupNumbers = SccComponenets.Count + 1;
+            foreach (int vertex in group)
+                vertex_low[vertex] = groupNumbers;
+
+            SccComponenets.Add(group);
+        }
+        */
 
         public bool SameComponent(int v1, int v2)
         {
---
+++ SAT_Problem.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using AlgorithmsLibrary.GraphAlgorithms;
+
+namespace AlgorithmsLibrary
+{
+    class SAT_Problem
+    {
+        public static void solve()
+        {
+            for (int f = 1; f <= 6; f++)
+            {
+                StronglyConnectedComponents scc = new StronglyConnectedComponents();
+                StreamReader sr = new StreamReader(@"C:\Users\Feras\Desktop\sat" + f + ".txt");
+                List<List<int>> adj = new List<List<int>>();
+                List<List<int>> adjTranspose = new List<List<int>>();
+                int n = int.Parse(sr.ReadLine());
+                int[] seen = new int[n];
+
+                for (int i = 0; i < 2 * n; i++)
+                {
+                    adj.Add(new List<int>());
+                    adjTranspose.Add(new List<int>());
+                }
+
+                int a, b, _a, _b;
+                string[] str;
+                while (!sr.EndOfStream)
+                {
+                    str = sr.ReadLine().Split(' ');
+                    a = int.Parse(str[0]);
+                    b = int.Parse(str[1]);
+                    seen[Math.Abs(a) - 1]++;
+                    seen[Math.Abs(b) - 1]++;
+                }
+
+                sr.Close();
+                sr = new StreamReader(@"C:\Users\Feras\Desktop\sat" + f + ".txt");
+                
+                sr.ReadLine();
+                while(!sr.EndOfStream)
+                {
+                    str = sr.ReadLine().Split(' ');
+                    a = int.Parse(str[0]);
+                    b = int.Parse(str[1]);
+
+                    if (seen[Math.Abs(a) - 1] == 1 || seen[Math.Abs(b) - 1] == 1)
+                        continue;
+
+                    if (a < 0)
+                    {
+                        a = (Math.Abs(a) - 1) + n;
+                        _a = a - n;
+                    }
+                    else
+                    {
+                        a = a - 1;
+                        _a = a + n;
+                    }
+
+                    if (b < 0)
+                    {
+                        b = (Math.Abs(b) - 1) + n;
+                        _b = b - n;
+                    }
+                    else
+                    {
+                         b = b - 1;
+                        _b = b + n;
+                    }
+
+                    adj[_a].Add(b);
+                    if(b != a && _a != _b)
+                        adj[_b].Add(a);
+                    /*
+                    // Add Edge Transpose
+                    adjTranspose[b].Add(_a);
+                    if (b != a && _a != _b)
+                        adjTranspose[a].Add(_b);
+                     * */
+                }
+
+                //scc.RunKosaraju(adj, adjTranspose, n);
+                scc.RunTarjan(adj, 2 * n);
+
+                bool satisfiable = true;
+                for (int i = 0; i < n && satisfiable; i++)
+                {
+                    satisfiable = !scc.SameComponent(i, i + n);
+                    if (!satisfiable)
+                        Console.WriteLine(i);
+                }
+
+                Console.WriteLine(satisfiable);
+                //scc.PrintComponents();
+                sr.Close();
+            }
+        }
+    }
+}
